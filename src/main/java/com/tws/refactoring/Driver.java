2ca31df57de89d6c59f00f2393ee0c8aa0d80874
WUPA2 <panda.wu@oocl.com>
2019-07-15 14:08:25 +0800

    defined ADULT_LIMIT and refactor checkDriver()

--- src/main/java/com/tws/refactoring/Driver.java
+++ src/main/java/com/tws/refactoring/Driver.java
@@ -2,7 +2,7 @@ package com.tws.refactoring;
 
 public class Driver {
     public int age;
-
+    public static final Integer ADULT_LIMIT=18;
     public Driver(int age) {
         this.age = age;
     }
